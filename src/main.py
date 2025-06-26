e07909f84788713c685ce6a2c582e17e153e4f83
dumiduzee <dumidulk@outlook.com>
2025-06-26 23:18:56 +0530

    feat: added global exception handler

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from database.database import Base,engine
 from api.users.routes import router as UserRouter
 from api.hostel.routes import router as HostelRouter
 from database.database import Base
 from api.users.models import UserModel
+from api.users.exceptions import UserExceptions
 
 
 app = FastAPI(
@@ -20,6 +22,19 @@ app = FastAPI(
 #Use routes
 app.include_router(prefix="/api/v1/user",router=UserRouter)
 app.include_router(prefix="/api/v1/hostel",router=HostelRouter)
+
+
+#custom exception handler
+@app.exception_handler(UserExceptions)
+async def exception_handler(request,exe:UserExceptions):
+    return JSONResponse(
+        status_code=exe.status_code,
+        content={
+            "error_code":exe.status_code,
+            "message":exe.detail,
+            "solution":exe.solution
+        }
+    )
 
 
 
